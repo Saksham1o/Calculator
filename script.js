5665fc9e38a621740c41f9c274913293b2760767
Saksham1o <sakshampatiyar26@gmail.com>
2024-07-18 19:10:08 +0530

    adding style and logic

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
 let displayValue = "";
 
-function appendtoDisplay(val) {
-    displayValue += val;
+function appendToDisplay(val) {
+  displayValue += val;
+  document.getElementById("display").value = displayValue;
+}
+
+function clearDisplay() {
+  displayValue = "";
+  document.getElementById("display").value = "";
+}
+
+function operate(operator) {
+  appendToDisplay(" " + operator + " ");
+}
+
+function calculate() {
+  try {
+    const result = eval(displayValue);
+    displayValue = result.toString();
     document.getElementById("display").value = displayValue;
-}
-
-function clearDiplay() {
-    displayValue = "";
-    document.getElementById("display").value = "";
-}
-
-function operate() {
-    appendtoDisplay(" " + operator + " ");
+  } catch (error) {
+    displayValue = "Error";
+    document.getElementById("display").value = displayValue;
+  }
 }
